36dd56651657416419c8c5c22d5df3dac1899c4d
Dmitriy Khmaladze <d@zhabis.com>
2023-09-29 15:44:18 -0400

    Add azos package

---
+++ src/azos/README.md
@@ -0,0 +1,1 @@
+# azos-js Core Package
---
+++ src/azos/index.js
@@ -0,0 +1,25 @@
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+// Root azos-js Export Index
+
+import * as coreconsts    from "./coreconsts";
+import * as aver          from "./aver";
+import * as types         from "./types";
+import * as strings       from "./strings";
+import * as localization  from "./localization";
+import * as events        from "./events";
+import * as linq          from "./linq";
+
+// These entities will be exported using UMD library mechanisms
+
+export const CoreConsts   = coreconsts;
+export const Aver         = aver;
+export const Types        = types;
+export const Strings      = strings;
+export const Localization = localization;
+export const Events       = events;
+export const Linq         = linq;
