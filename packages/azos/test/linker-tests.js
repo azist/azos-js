f44581d9cec86dee75e0f6353d90b5b6ce6eaead
Dmitriy Khmaladze <d@zhabis.com>
2023-10-24 19:40:43 -0400

    Unit tests linker

--- packages/azos/test/linker-tests.js
+++ packages/azos/test/linker-tests.js
@@ -75,6 +75,59 @@ describe("Linker", function() {
       aver.areEqual("Nationwide weather is fair; 75F: 123", nationWeather.getWeather(123));
     });
 
+    it("concrete type",   function() {
+      const linker = new mod.ModuleLinker();
+      const cfg = conf.config({}).root;
+      aver.isTrue(  linker.register(new LocalWeather(apps.NopApplication.instance, cfg), IWeather)  );
+      aver.isTrue(  linker.register(new NationWeather(apps.NopApplication.instance, cfg), NationWeather)  );
+
+      const localWeather = linker.resolve(IWeather);
+      const nationWeather = linker.resolve(NationWeather);
+
+      aver.isNotNull(localWeather);
+      aver.isNotNull(nationWeather);
+
+      aver.isOf(localWeather, IWeather);
+      aver.isOf(localWeather, LocalWeather);
+
+      aver.isOf(nationWeather, IWeather);
+      aver.isOf(nationWeather, NationWeather);
+
+      aver.areEqual("Local weather is cloudy: Z891", localWeather.getWeather("Z891"));
+      aver.areEqual("Nationwide weather is fair; 75F: 122", nationWeather.getWeather(122));
+    });
+
+    it("concrete type with name",   function() {
+      const linker = new mod.ModuleLinker();
+      const cfg = conf.config({}).root;
+      aver.isTrue(  linker.register(new LocalWeather(apps.NopApplication.instance, cfg), IWeather, "loc")  );
+      aver.isTrue(  linker.register(new NationWeather(apps.NopApplication.instance, cfg), NationWeather, "nat")  );
+
+      const localWeather = linker.resolve(IWeather, "loc");
+      const nationWeather = linker.resolve(NationWeather, "nat");
+
+      aver.isNotNull(localWeather);
+      aver.isNotNull(nationWeather);
+
+      aver.isOf(localWeather, IWeather);
+      aver.isOf(localWeather, LocalWeather);
+
+      aver.isOf(nationWeather, IWeather);
+      aver.isOf(nationWeather, NationWeather);
+
+      aver.areEqual("Local weather is cloudy: Z8900", localWeather.getWeather("Z8900"));
+      aver.areEqual("Nationwide weather is fair; 75F: 1225", nationWeather.getWeather(1225));
+
+      aver.isNotNull(linker.tryResolve(IWeather, "loc"));
+      aver.isNull(linker.tryResolve(LocalWeather, "loc"));//because of concrete type
+      aver.isNull(linker.tryResolve(LocalWeather, "nat"));//because of name mismatch
+
+      aver.isNull(linker.tryResolve(IWeather, "nat"));//because of concrete type
+      aver.isNotNull(linker.tryResolve(NationWeather, "nat"));
+      aver.isNull(linker.tryResolve(NationWeather, "loc"));//because of name mismatch
+
+    });
+
   });
 
 });
