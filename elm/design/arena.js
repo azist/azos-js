1399db086aa4b76771786fbeb085d1d87da8dae7
Dmitriy Khmaladze <d@zhabis.com>
2024-01-14 22:28:53 -0500

    Css wip

---
+++ elm/design/arena.js
@@ -0,0 +1,15 @@
+//If this script is the LAST line before </body> then there is no need in DOMContentLoaded
+//document.addEventListener('DOMContentLoaded', () => {
+
+const openMenu  = () => document.getElementById("navMenu").classList.add("side-menu_expanded");
+const closeMenu = () => document.getElementById("navMenu").classList.remove("side-menu_expanded");
+
+const btnMenuOpen = document.getElementById("btnMenuOpen");
+const btnMenuClose = document.getElementById("btnMenuClose");
+
+btnMenuOpen.addEventListener("click", openMenu);
+btnMenuClose.addEventListener("click", closeMenu);
+
+document.addEventListener("click", e => { if (!btnMenuOpen.contains(e.target)) closeMenu(); });
+
+//});
