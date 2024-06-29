315189a652b30f87f40b1c4021451c926319ca31
Dmitriy Khmaladze <d@zhabis.com>
2024-06-28 21:30:41 -0400

    Button styling experiemnts

--- packages/azos-ui/parts/button.js
+++ packages/azos-ui/parts/button.js
@@ -8,13 +8,27 @@ export class Button extends AzosPart{
   static styles = [baseStyles, css`
   button{
     border: 1px solid gray;
+    background: linear-gradient(180deg, white, #e8e8e8);
+    box-shadow: 0px 0px 2px rgba(25, 25, 25, .25);
     padding: 0.5lh 1ch 0.5lh 1ch;
     margin: 0.5lh 0.5ch 0.5lh 0.5ch;
     min-width: 10ch;
+    transition: 0.2s;
   }
-    button:hover{
-      filter:  brightness(1.15);
-    }
+
+  button:focus{
+    outline: 1px solid rgba(10, 128, 255, 0.55);
+    box-shadow: 0px 0px 8px rgba(10, 128, 255, .9);
+  }
+
+  button:hover{
+    filter:  brightness(1.08);
+  }
+
+  button:active{
+    transform: translateY(2px);
+  }
+
   `];
 
   static properties = {
