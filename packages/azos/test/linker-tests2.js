3b05a5012dd4b931ca958bcf29af1de559810416
Kevin Biesbrock <kbiesbrock@gmail.com>
2024-09-04 10:48:29 -0400

    whitespace after formatting file -kb

--- packages/azos/test/linker-tests2.js
+++ packages/azos/test/linker-tests2.js
@@ -7,7 +7,7 @@
 //import { describe, it } from "mocha";
 import { defineUnit as describe, defineCase as it } from "../run.js";
 import * as aver from "azos/aver";
-import {ABSTRACT} from "azos/coreconsts";
+import { ABSTRACT } from "azos/coreconsts";
 import * as conf from "azos/conf";
 import * as apps from "azos/application";
 //import * as cmp from "azos/components";
@@ -15,40 +15,40 @@ import * as mod from "azos/modules";
 import * as sut from "azos/linker";
 
 
-class IWeather extends mod.Module{
-  constructor(dir, cfg){ super(dir, cfg); }
+class IWeather extends mod.Module {
+  constructor(dir, cfg) { super(dir, cfg); }
   // eslint-disable-next-line no-unused-vars
-  getWeather(code){ throw ABSTRACT("IWeather.getWeather()"); }
+  getWeather(code) { throw ABSTRACT("IWeather.getWeather()"); }
 }
 
-class LocalWeather extends IWeather{
-  constructor(dir, cfg){ super(dir, cfg); }
-  getWeather(code){ return "Local weather is cloudy: " + (code ?? "none"); }
+class LocalWeather extends IWeather {
+  constructor(dir, cfg) { super(dir, cfg); }
+  getWeather(code) { return "Local weather is cloudy: " + (code ?? "none"); }
 }
 
-class NationWeather extends IWeather{
-  constructor(dir, cfg){ super(dir, cfg); }
-  getWeather(code){ return "Nationwide weather is fair; 75F: "+ (code ?? "none");  }
+class NationWeather extends IWeather {
+  constructor(dir, cfg) { super(dir, cfg); }
+  getWeather(code) { return "Nationwide weather is fair; 75F: " + (code ?? "none"); }
 }
 
 
-describe("Linker Additional Tests", function() {
+describe("Linker Additional Tests", function () {
 
-  describe("ModuleLinker", function() {
+  describe("ModuleLinker", function () {
 
     /**
      * When multiple services are registered under the same name (or name=null), use the first
      *  registered service.
      */
-    it("uses-first-registered-service",   function() {
+    it("uses-first-registered-service", function () {
       const linker = new mod.ModuleLinker();
       const cfg = conf.config({}).root;
 
       const modLocal = new LocalWeather(apps.NopApplication.instance, cfg);
       const modNational = new NationWeather(apps.NopApplication.instance, cfg);
 
-      aver.isTrue(  linker.register(modNational)  );
-      aver.isTrue(  linker.register(modLocal)  );
+      aver.isTrue(linker.register(modNational));
+      aver.isTrue(linker.register(modLocal));
 
       const ref = {
         weather: IWeather
@@ -64,15 +64,15 @@ describe("Linker Additional Tests", function() {
      * When multiple services are attempted to be registered under the same name, throws an
      *  exception giving developer advanced notice of an unlikely (unexpected) outcome.
      */
-    it("throws-when-registering-second-service", function() {
+    it("throws-when-registering-second-service", function () {
       const linker = new mod.ModuleLinker();
       const cfg = conf.config({}).root;
 
       const modLocal = new LocalWeather(apps.NopApplication.instance, cfg);
       const modNational = new NationWeather(apps.NopApplication.instance, cfg);
 
-      aver.isTrue(  linker.register(modLocal)  );
-      aver.throws(  ()=> linker.register(modNational), "register"  );
+      aver.isTrue(linker.register(modLocal));
+      aver.throws(() => linker.register(modNational), "register");
 
     });
 
