a820da4d1433815ef3a5c9a986e5fef308e8a4ad
Shawn Gray <shawn.gray@greatdayimprovements.com>
2024-11-27 14:20:30 -0500

    WIP Repeater

--- packages/azos-ui/vcl/util/repeater.js
+++ packages/azos-ui/vcl/util/repeater.js
@@ -1,7 +1,42 @@
 import { AzosElement } from "../../ui";
+import "../../parts/text-field";
 
 export class Repeater extends AzosElement {
+
+  static properties = {
+    /*
+      items={
+        {
+          "id":"",
+          "title":"",
+          "value":""
+        },{
+          "id":"",
+          "title":"",
+          "value":""
+        }
+      }
+    */
+    items: { type: Object }
+  }
+
   constructor() { super(); }
+
+  render() {
+    const itemList = html`${this.items.map((item) => {
+      return html`
+        <az-text
+          id="${item.id}"
+          scope="this"
+          name="${item.id}"
+          title="${item.title}"
+          value="${item.value}"
+        >
+        </az-text>
+      `;
+    })}`;
+    return html`${itemList}`;
+  }
 }
 
 window.customElements.define("az-repeater", Repeater);
