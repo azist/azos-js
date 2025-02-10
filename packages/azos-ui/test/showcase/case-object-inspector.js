025f8c380c78efa790dea9dfce690f99b30e7c16
Kevin Biesbrock <kbiesbrock@gmail.com>
2025-02-10 10:05:46 -0500

    toggle different result structures, testing -kb

--- packages/azos-ui/test/showcase/case-object-inspector.js
+++ packages/azos-ui/test/showcase/case-object-inspector.js
@@ -10,12 +10,19 @@ import { CaseBase } from "./case-base";
 import "../../vcl/util/object-inspector";
 
 export class CaseObjectInspector extends CaseBase {
-  results = [{ key1: "value" }, { key2: { childKey1: true, childKey2: 5 } }, { key3: [{ childKey3: false, childKey4: 85 }] }];
-  obj = { a: true, b: 1, c: "Moo", d: ['d0', 'd1', 'd2', 'd3'], e: { e0: true, e1: 1, e2: "Cow", e3: [{ e30: true, e31: 31, e32: "Great", e33: ["e330", "e331", "e332", "e333"] }] } }
+  arr = [{ key1: "value" }, { key2: { childKey1: true, childKey2: 5 } }, { key3: [{ childKey3: false, childKey4: 85 }] }];
+  obj = { a: true, b: 1, c: "Moo", d: ['d0', 'd1', 'd2', 'd3'], e: { e0: true, e1: 1, e2: "Cow", e3: [{ e30: true, e31: 31, e32: "Great", e33: ["e330", "e331", "e332", "e333"] }] } };
+
+  onRadioChange(evt) { this.objectInspector.doc = evt.target.value === "1" ? this.arr : this.obj; }
 
   renderControl() {
     return html`
 <h2>Object Inspector</h2>
+
+<az-radio-group title="Show which Object?" @change="${this.onRadioChange}" titlePosition="mid-left" value="2">
+  <item title="Show Array" value="1"></item>
+  <item title="Show Object"  value="2"></item>
+</az-radio-group>
 
 <az-object-inspector id="objectInspector" scope="this" .doc=${this.obj}></az-object-inspector>
     `;
