7e269a7853452c51dbbdd24cd0a6dc0a2061d7f7
Dmitriy Khmaladze <d@zhabis.com>
2025-03-25 16:19:23 -0400

    Mru

---
+++ packages/azos-ui/mru.js
@@ -0,0 +1,88 @@
+import { Module } from "azos/modules";
+import { IStorage } from "azos/storage";
+import { Applet } from "./applet";
+import { isFunctionOrNull, isNonEmptyString, isObjectOrArray, isOf, isOfOrNull, isStringOrNull } from "azos/aver";
+import { keepBetween } from "azos/types";
+import { matchPattern } from "azos/strings";
+
+/** Provides Most Recently Used named list management functionality in a UI application */
+export class MruLogic extends Module{
+
+  #ref = {storage: IStorage};
+  #max = 25;
+
+  constructor(dir, cfg){
+    super(dir, cfg);
+    this.#max = keepBetween(cfg.getInt("max", 25), 1, 250);
+  }
+
+  _appAfterLoad(){
+    super._appAfterLoad();
+    this.link(this.#ref);
+  }
+
+  #getKeyName(applet, idList){ return `MRU::${(applet?.localStoragePrefix ?? "*")}::${idList ?? "*"}`;}
+
+  /** Clears all MRU lists optionally filtering by applet and MRU list id */
+  clearAll(applet = null, idList = null){
+    isOfOrNull(applet, Applet);
+    isStringOrNull(idList);
+    const toDrop = [];
+
+    const pattern = this.#getKeyName(applet, idList);
+    for(let i=0; i < this.#ref.storage.length; i++){
+      const key = this.#ref.key(i);
+      if (!matchPattern(key, pattern)) continue;
+      toDrop.push(key);
+    }
+
+    for(const k of toDrop) this.#ref.storage.removeItem(k);
+  }
+
+  /** Returns the most recently used items by MRU list id an applet, e.g. `filter`.
+   * Returns empty array if such MRU list is not found by id
+   */
+  getMruList(applet, idList){
+    isOf(applet, Applet);
+    isNonEmptyString(idList, String);
+    const fullKey = this.#getKeyName(applet, idList);
+    const stored = this.#ref.storage.getItem(fullKey);
+    return stored ?? [];
+  }
+
+  /** Pushes an item into the storage. Add the item to the head (at the beginning) of the list.
+   * The item identity is defined by a supplied comparer `fn(a, b): bool`
+   *
+   * @example
+   *  mruLogic.putMruListItem(thisApplet, "filter", {id: 278, s: "Harris Joe*", now}, (a,b) => a.id === b.id);
+   *  mruLogic.putMruListItem(thisApplet, "filter", {s: "Harris Joe*"}, (a,b) => matchPattern(a.s, `*.${b.s}*`);
+  */
+  putMruListItem(applet, idList, item, fItemIdentityComparer = null){
+    const list = this.getMruList(applet, idList);
+    isObjectOrArray(item);
+    isFunctionOrNull(fItemIdentityComparer);
+
+    if (fItemIdentityComparer){
+      let iMatch = -1;
+      for(let i =0; i < list.length; i++){
+        if (fItemIdentityComparer(list[i], item)){
+          iMatch = i;
+          break;
+        }
+      }
+      if (iMatch >=0 ) {
+        list.splice(iMatch, 1);
+      }
+    }
+
+    list.unshift(item);//insert up front
+
+
+    if (list.length > this.#max) list.pop();//impose a limit
+
+    const fullKey = this.#getKeyName(applet, idList);
+    this.#ref.storage.setItem(fullKey, list);
+    return list;
+  }
+
+}
