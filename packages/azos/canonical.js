e58513b2917c49262ba9dae99c36f31153c6667c
Kevin Biesbrock <kbiesbrock@gmail.com>
2025-04-15 10:49:13 -0400

    Changes per PR #356 -kb

---
+++ packages/azos/canonical.js
@@ -0,0 +1,4 @@
+import { Atom } from "./atom";
+
+export const ATM_SCH_GDID = Atom.encode("gdid");
+export const ATM_SCH_ID = Atom.encode("id");
--- packages/azos/types.js
+++ packages/azos/types.js
@@ -879,23 +879,6 @@ export const TYPE_MONIKER = Object.freeze({
 });
 const ALL_TYPE_MONIKERS = allObjectValues(TYPE_MONIKER);
 
-export const EID_ROOT_USER = EntityId.parse("usrn@idp::root");
-
-export const ATM_SYS_AUTHKIT = Atom.encode("sky-auth");
-
-export const ATM_SCH_GDID = Atom.encode("gdid");
-export const ATM_SCH_ID = Atom.encode("id");
-
-export const ATM_ETP_USER = Atom.encode("user");
-export const ATM_ETP_LOGIN = Atom.encode("login");
-export const ATM_ETP_ORGUNIT = Atom.encode("orgu");
-
-export const ATM_LTP_SYS_EMAIL = Atom.encode("email");
-export const ATM_LTP_SYS_PHONE = Atom.encode("phone");
-export const ATM_LTP_SYS_ID = Atom.encode("id");
-export const ATM_LTP_SYS_SCREEN_NAME = Atom.encode("screenm");
-export const ATM_LTP_SYS_URI = Atom.encode("uri");
-
 /**
  * Converts value into a valid TYPE_MONIKER member
  * @param {*} v string moniker
