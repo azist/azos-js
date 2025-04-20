20e2030fb80c3aaea4761e2706255af57506f6e4
Dmitriy Khmaladze <d@zhabis.com>
2025-04-19 20:52:44 -0400

    #360 Add nested testing

--- packages/azos-ui/test/examples/person-data.js
+++ packages/azos-ui/test/examples/person-data.js
@@ -34,6 +34,10 @@ export class PersonBlock extends Block{
       <az-text scope="this"  id="tbLastName"     name="LastName"   title="Last Name"   maxLength=16 isrequired value="Cabbage"></az-text>
       <az-check scope="this" id="chkRegistered"  name="Registered" title="Registered"  isrequired value="true" ></az-check>
       <az-check scope="this" id="chkSmoker"      name="Smoker" title="Former Smoker"  isrequired value="false"></az-check>
+      <h4>Processing Status</h4>
+      <examples-status-block scope="this" id="blockProcessStatus" name="ProcessStatus"></examples-status-block>
+      <h4>Payout Status</h4>
+      <examples-status-block scope="this" id="blockPayoutStatus" name="PayoutStatus"></examples-status-block>
     `;
   }
 
@@ -46,4 +50,15 @@ export class PersonBlock extends Block{
   }
 }
 
+export class StatusBlock extends Block{
+  render(){
+    return html`
+      <az-text scope="this"  id="tbStatus"    name="Status"  title="Status"  maxLength=10 isrequired value="Init"></az-text>
+      <az-text scope="this"  id="tbDescription"   name="Description" title="Description" maxLength=25  value="Initital" ></az-text>
+      <az-check scope="this" id="chkApproved" checktype="switch" status="warning" name="Approved" title="Approved"  isrequired value="false" ></az-check>
+    `;
+  }
+}
+
 window.customElements.define("examples-person-block", PersonBlock);
+window.customElements.define("examples-status-block", StatusBlock);
