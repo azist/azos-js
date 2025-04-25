45e7eaba321673dcb11fbc671d0bae5985128038
Dmitriy Khmaladze <d@zhabis.com>
2025-04-24 23:43:15 -0400

    #360 Introduce CrudForm and FeatureD test applet

---
+++ packages/azos-ui/crud-form.js
@@ -0,0 +1,137 @@
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+import { DATA_MODE, DATA_MODE_PROP, DATA_VALUE_PROP, ERROR_PROP, VALIDATE_METHOD, VISIT_METHOD } from "azos/types";
+import { Form, Block } from "./blocks.js";
+import { css, html } from "./ui.js";
+import { showMsg } from "./msg-box.js";
+import { isOneOf } from "azos/strings";
+
+/**
+ * Provides {@link Form} specialization for CRUD -related data functionality
+ **/
+export class CrudForm extends Form {
+
+  //todo: Work with Arron to use his universal block/form styles
+  static styles = [Block.styles, css``];
+
+  static properties = {
+    toolbar: {type: String}
+  };
+
+  get isToolbarAbove(){ return isOneOf(this.toolbar, ["top", "above"]); }
+
+  #data = null;
+  #saveResult;
+
+
+  /** Captures an object (if any) returned upon save */
+  get saveResult(){ return this.#saveResult; }
+  /** Captures an object (if any) returned upon save */
+  set saveResult(v){ this.#saveResult = v; }
+
+
+  firstUpdated(){
+    super.firstUpdated();
+    this[DATA_MODE_PROP] = DATA_MODE.UNSPECIFIED;
+    this.applyInvariants();
+  }
+
+  #btnNewClick(){
+    this[DATA_VALUE_PROP] = null; //todo:  SET DEFAULT here
+    this[DATA_MODE_PROP] = DATA_MODE.INSERT;
+    this.applyInvariants();
+  }
+
+  #btnEditClick(){
+    this[DATA_MODE_PROP] = DATA_MODE.UPDATE;
+    this.applyInvariants();
+  }
+
+
+  async #btnSaveClick(){
+    const errors = this[VALIDATE_METHOD]({}, null, true);
+
+    //todo: Create a usable Validation Summary browser
+    if (errors) {
+      showMsg("error", "Validation Errors", "Error list: \n\n" +JSON.stringify(errors, null, 2), 3, true);
+      return;
+    }
+
+    showMsg("ok", "Saved Data", "The following is obtained \n by calling [DATA_VALUE_PROP]: \n\n" +JSON.stringify(this[DATA_VALUE_PROP], null, 2), 3, true);
+
+    this.#saveResult = await this._doSave();
+
+    this.#data = this[DATA_VALUE_PROP];//commit data into the buffer
+    this[DATA_MODE_PROP] = DATA_MODE.UNSPECIFIED;
+    this.applyInvariants();
+  }
+
+  /**
+   * Override to perform actual work, such as service client or logic call.
+   * The data is already client-validated before this call
+   * Important: upon a successful save you can return/set some values into hidden fields, or `saveResult`
+   * property -  this way you can pass extra data back, such as server-assigned GDID or save message
+   * @returns {any} saveResult object, such as save message etc...
+   * */
+  async _doSave(){
+
+  }
+
+  async #btnCancelClick(){
+
+    //todo:  Query isDirty and abort if there are changes
+
+    //reset errors which do not matter as we cancel
+    this[VISIT_METHOD]( one => {
+      if (ERROR_PROP in one) one[ERROR_PROP] = null;
+    });
+
+    //set back to "View"
+    this[DATA_MODE_PROP] = DATA_MODE.UNSPECIFIED;
+    this[DATA_VALUE_PROP] = this.#data;
+
+    this.applyInvariants();
+  }
+
+  /**
+   * Override to apply invariant logical rules, such as button enabled/disabled states etc.
+   * This method is called on every form mode transition, such as the one after NEW/EDIT/SAVE/CANCEL button clicks
+   */
+  applyInvariants(){
+    const mode = this[DATA_MODE_PROP];
+    const isView = mode === undefined || mode === DATA_MODE.UNSPECIFIED;
+    this.btnNew.isEnabled = isView;
+    this.btnEdit.isEnabled = isView;
+    this.btnSave.isDisabled = isView;
+    this.btnSave.status = isView ? "default" : "info";
+    this.btnCancel.isDisabled = isView;
+    this.btnCancel.status = isView ? "default" : "alert";
+  }
+
+
+  renderControl(){
+    return this.isToolbarAbove ? html` ${this.renderToolbar()} ${this.renderFormBody()} `
+                               : html` ${this.renderFormBody()} ${this.renderToolbar()} `;
+  }
+
+  renderToolbar(){
+    return html`
+    <div class="toolbar">
+       <az-button id="btnNew" scope="this" @click="${this.#btnNewClick}" title="New"></az-button>
+       <az-button id="btnEdit" scope="this" @click="${this.#btnEditClick}" title="Edit"></az-button>
+       <az-button id="btnSave" scope="this" @click="${this.#btnSaveClick}" title="Save"></az-button>
+       <az-button id="btnCancel" scope="this" @click="${this.#btnCancelClick}" title="Cancel"></az-button>
+    </div>`;
+  }
+
+  renderFormBody(){
+    return html`<div class="form-body"> <slot>  </slot> </div>`;
+  }
+
+}
+
+window.customElements.define("az-crud-form", CrudForm);
---
+++ packages/azos-ui/test/examples/featureD-applet.js
@@ -0,0 +1,37 @@
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+
+//import { Permission } from "azos/security";
+import { Applet } from "../../applet.js";
+import { css, html } from "../../ui.js";
+
+import "./person-blocks.js";
+import "../../parts/button.js";
+import "../../modal-dialog.js";
+import "../../crud-form.js";
+
+export class ExampleFeatureDApplet extends Applet{
+
+ ////Uncommenting this will require user principal to have that permission
+  //static permissions = [ new Permission("test", "Master", 5), {ns: "System", name: "UserManager", level: 500}];
+
+  static styles = css`:host{ padding: 1ch 2ch; display: block; }`;
+
+  get title(){ return "Feature D - CRUD Data Forms"; }
+
+  render(){
+   return html`
+     <az-crud-form id="frmMain" scope="this" toolbar="below" >
+
+       <examples-person-block scope="this" id="blockPerson" name="data"> </examples-person-block>
+
+     </az-crud-form>
+   `;
+  }
+}
+
+window.customElements.define("examples-featured-applet", ExampleFeatureDApplet);
