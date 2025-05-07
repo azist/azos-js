727b0f37ee9117dcafd42bc8899d9bb8f75a5965
Kevin Biesbrock <kbiesbrock@gmail.com>
2025-05-07 09:04:56 -0400

    Resolves #405 rename consts to "inline grid",  css class to "row" -kb

--- packages/azos-ui/styles/grid.js
+++ packages/azos-ui/styles/grid.js
@@ -1,43 +1,43 @@
-import { css } from "../ui"; 
+import { css } from "../ui";
 import { BREAKPOINT_SM } from "./breakpoints";
 /**
  * Provides a basic grid system
  */
-const STL_GRID = css`
-  .grid {
+const STL_INLINE_GRID = css`
+  .row {
     display: grid;
     gap: 1em;
     margin-bottom: 1em;
   }
- 
+
   .cols1 {
     grid-template-columns: repeat(1, 1fr);
   }
- 
+
   .cols2 {
     grid-template-columns: repeat(2, 1fr);
   }
- 
+
   .cols3 {
     grid-template-columns: repeat(3, 1fr);
   }
- 
+
   .cols4 {
     grid-template-columns: repeat(4, 1fr);
   }
- 
+
   .span2 {
     grid-column: span 2;
   }
- 
+
   .span3 {
     grid-column: span 3;
   }
- 
+
   .span4 {
     grid-column: span 4;
   }
- 
+
   @media (max-width: ${BREAKPOINT_SM}px) {
     .cols2, .cols3, .cols4 {
       grid-template-columns: repeat(1, 1fr);
@@ -45,4 +45,4 @@ const STL_GRID = css`
   }
 `;
 
-export default STL_GRID;
+export default STL_INLINE_GRID;
