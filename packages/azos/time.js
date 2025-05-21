adfdb3acf0880ab5797cfe391d2e05fc188ea7a7
Dmitriy Khmaladze <d@zhabis.com>
2025-05-21 18:11:57 -0400

    #278 Add Time.js wip

---
+++ packages/azos/time.js
@@ -0,0 +1,188 @@
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+import * as aver from "./aver.js";
+import { Module } from "./modules.js";
+
+/** Provides named time zone information along with ability to convert UTC timestamps to local (as of timezone) components and back */
+export class TimeZone {
+
+  #name;
+  #description;
+  #ianaName;
+  #windowsName;
+
+  constructor(cfg){
+
+  }
+
+  /** Gets standard offset of this time zone relative to UTC */
+  get standardBaseOffsetMs(){ return 0; }
+
+  /**
+   * Returns the millisecond offset of this timezone relative to UTC as of the specified UTC timestamp
+   * @param {number| Date} ts number or date UTC timestamp
+   * @returns {number} offset in milliseconds
+  */
+  getOffsetMsAsOfUtc(ts){ return 0; }
+
+  /**
+   * Returns the millisecond offset of this timezone relative to UTC as of LOCAL (for this zone) timestamp with explicit DST flag
+   * @param {number| Date} ts number or date LOCAL (as of this zone)UTC timestamp
+   * @param {Boolean} isDST true when the date is in Daylight Saving Time mode
+   * @returns {number} offset in milliseconds
+  */
+  getOffsetMsAsOfLocal(ts, isDST){ return 0; }
+
+
+  /** Extracts components out of UTC timestamp as of THIS timezone,
+   * that is: you pass-in a UTC timestamp as a number of milliseconds since UNIX epoch, or a Date object (which is already based UX epoch timestamp internally),
+   * the system calculates the offset as of that timestamp and applies it to the argument, effectively returning components of the
+   * date value as of THIS timezone
+   * @param {number| Date} ts number or date UTC timestamp
+  */
+  extractComponents(ts){
+    if (ts === null || ts == undefined) ts = 0;
+    else if (ts instanceof Date) ts = ts.getTime();
+    else aver.isNumber(ts, "ts must be a number");
+
+    const offset = this.getOffsetMsAsOfUtc(ts);
+    const result = new Date(ts + offset);// e.g. CLE is -5:0, so we add -5 hours to UTC to get local time
+
+    //note: we use Date as a convenience, the local date is irrelevant and its UTC component is really wht we need
+    //this is why it is an implementation detail and not a part of the public API
+    return {
+      year:        result.getUTCFullYear(),
+      month:       result.getUTCMonth() + 1,
+      day:         result.getUTCDate(),
+      dayw:        result.getUTCDay() + 1,
+      hour:        result.getUTCHours(),
+      minute:      result.getUTCMinutes(),
+      second:      result.getUTCSeconds(),
+      millisecond: result.getUTCMilliseconds()
+    };
+  }
+
+  /** Converts components of a date as of THIS timezone (LOCAL) to UTC timestamp */
+  combineComponents(components){
+    aver.isObject(components, "components must be an object");
+    const { year, month, day, hour, minute, second, millisecond, isUtc, isDST } = components;
+    const ts = Date.UTC(year, month - 1, day, hour, minute, second, millisecond);
+    if (isUtc) return ts; // no conversion needed
+
+    const offset = this.getOffsetMsAsOfLocal(ts, isDST);
+    return ts - offset;// e.g. CLE is -5:0, so we subtract -5 hours from LOCAL to get to UTC
+  }
+
+
+}
+
+/** Encapsulates general US and Canada Daylight savings conversion rules */
+export class UsStandardTimeZone extends TimeZone {
+/*
+    United States and Canada
+    DST begins at 2:00 a.m. local time on the second Sunday in March.
+    DST ends at 2:00 a.m. local time on the first Sunday in November.
+    During the start, clocks "spring forward" from 2:00 a.m. to 3:00 a.m., and during the end, clocks "fall back" from 2:00 a.m. to 1:00 a.m.
+    Exceptions in the US include Arizona (except Navajo Nation), Hawaii, and some territories which do not observe DST
+*/
+   static DST_OFFSET_MS = 1  * //hr
+                          60 * //min
+                          60 * //sec
+                          1000; //ms
+
+
+  #getDstRange(ldt){
+    let dstStart = new Date(DataTransfer.UTC(ldt.getUTCFullYear, 3/*March*/, 1, 2, 0, 0));
+    for(let sun = 0;;){
+      if (dstStart.getUTCDay() === 0) {//Sunday == 0
+        sun++;
+      }
+      if (sun == 2) break;
+      dstStart.setDate(dstStart.getDate() + 1);
+    }
+
+    let dstEnd = new Date(DataTransfer.UTC(ldt.getUTCFullYear, 11/*November*/, 1, 2, 0, 0));
+    while(dstEnd.getUTCDay() !== 0) dstEnd.setDate(dstEnd.getDate() + 1);
+
+    return { start: dstStart, end: dstEnd };
+  }
+
+  /**
+   * Returns the millisecond offset of this timezone relative to UTC as of the specified UTC timestamp
+   * @param {number| Date} ts number or date UTC timestamp
+   * @returns {number} offset in milliseconds
+  */
+  getOffsetMsAsOfUtc(ts){
+    const so = this.standardBaseOffsetMs;
+
+    const lts = ts + so; // local time
+    const ldt = new Date(lts);//fake "UTC" since Date does not have an API
+
+    let {dstStart, dstEnd} = this.#getDstRange(ldt);
+
+    const isDST = ldt >= dstStart && ldt < dstEnd;
+    const dstOffset = isDST ? UsStandardTimeZone.DST_OFFSET_MS : 0;
+
+    return so + dstOffset;
+  }
+
+  /**
+   * Returns the millisecond offset of this timezone relative to UTC as of LOCAL (for this zone) timestamp with explicit DST flag
+   * @param {number| Date} ts number or date LOCAL (as of this zone)UTC timestamp
+   * @param {Boolean} isDST true when the date is in Daylight Saving Time mode
+   * @returns {number} offset in milliseconds
+  */
+  getOffsetMsAsOfLocal(ts, isDst = true){    // Mar 8, 2023  2:am
+    const so = this.standardBaseOffsetMs;
+
+    const lts = ts; // already local time
+    const ldt = new Date(lts);//fake "UTC" since Date does not have an API
+
+    let {dstStart, dstEnd} = this.#getDstRange(ldt);
+
+    const isInDstRange = isDst && ldt >= dstStart && ldt < dstEnd;
+    const dstOffset = isInDstRange ? UsStandardTimeZone.DST_OFFSET_MS : 0;
+
+    return so + dstOffset;
+  }
+}
+
+
+/**
+ * Provides a module which provides a registry of named {@link TimeZone} instances}
+ */
+export class TimeZoneManager extends Module {
+  #map;
+  constructor() {
+    super();
+    this.#map = new Map();
+  }
+
+  getZone(zone){
+    const result = this.tryGetZone(zone);
+    if (result === null) {
+      throw new Error(`TimeZone ${zone} not found`);
+    }
+    return result;
+  }
+
+  tryGetZone(zone){
+    return null;
+  }
+}
+
+
+/*
+https://www.webexhibits.org/daylightsaving/b2.html
+
+Time zone to UTC conversion does not "know" which instance of a duplicated hour to use unless it is provided with enough context—specifically,
+ whether the time is during DST or standard time. Accurate conversion relies on explicit time zone and offset information, or else the result
+  is ambiguous and must be resolved by the application or user
+
+SEE THIS:  https://www.caktusgroup.com/blog/2019/03/21/coding-time-zones-and-daylight-saving-time/
+
+*/
