9a5c8c396301ff8694126344fedc7f3f1114e3e6
Dmitriy Khmaladze <d@zhabis.com>
2025-05-22 15:34:04 -0400

    #278 add unit test

--- packages/azos/test/time-tests.js
+++ packages/azos/test/time-tests.js
@@ -9,7 +9,7 @@ import * as aver from "azos/aver";
 import * as conf from "azos/conf";
 import * as apps from "azos/application";
 import { TimeZone, TimeZoneManager, TZ_UTC } from "../time.js";
-import { doUsing } from "../types.js";
+import { AzosError, doUsing } from "../types.js";
 
 
 class TeztTimeZone extends TimeZone {
@@ -20,12 +20,32 @@ class TeztTimeZone extends TimeZone {
 
 unit("Time", function () {
 
+  //$ npm test "*TimeZoneManager*"
   unit("TimeZoneManager", function () {
 
-    /**
-     * When multiple services are registered under the same name (or name=null), use the first
-     *  registered service.
-     */
+    cs("default-try-get", function () {
+      doUsing(apps.application({
+        modules: [{name: "tzm", type: TimeZoneManager}]
+      }), (app) => {
+        const tzm = app.moduleLinker.tryResolve(TimeZoneManager);
+        aver.isOf(tzm, TimeZoneManager, "TimeZoneManager is of the right type");
+
+        const utc = tzm.getZone(TZ_UTC);
+        aver.isOf(utc, TimeZone, "UTC is a TimeZone");//even when it is not declared, it is always present
+
+        const tryFake = tzm.tryGetZone("fake-zone");
+        aver.isNull(tryFake, "fake-zone is not present");
+
+        try{
+          tzm.getZone("fake-zone");
+          throw aver.AVERMENT_FAILURE("getZone should have thrown");
+        } catch (e) {
+          aver.isOf(e, AzosError, "getZone should have thrown AzosError");
+        }
+      });
+    });
+
+
     cs("default-utc-timezone", function () {
       doUsing(apps.application({
         modules: [{name: "tzm", type: TimeZoneManager}]
@@ -36,7 +56,9 @@ unit("Time", function () {
         const utc = tzm.getZone(TZ_UTC);
         aver.isOf(utc, TimeZone, "UTC is a TimeZone");
 
-        const got = utc.extractComponents(Date.UTC(1980, 0, 1, 14, 23, 41, 345));
+        const originalTs = Date.UTC(1980, 0, 1, 14, 23, 41, 345);
+
+        const got = utc.extractComponents(originalTs);
 
         aver.areEqual(1980, got.year, "year");
         aver.areEqual(1, got.month, "month");
@@ -45,6 +67,9 @@ unit("Time", function () {
         aver.areEqual(23, got.minute, "minute");
         aver.areEqual(41, got.second, "second");
         aver.areEqual(345, got.millisecond, "millisecond");
+
+        aver.areEqual(originalTs, utc.combineComponents({ year: 1980, month: 1, day: 1, hour: 14,  minute: 23, second: 41, millisecond: 345}));
+
       });
     });
 
