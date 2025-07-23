3131009665d0cf6858f83545d639b731bebe6658
Matthew O'Donnell <matthewjohnodonnell@gmail.com>
2025-07-23 16:47:16 -0400

    #473:wip schema-bit and showcase updated.

--- packages/azos-ui/models/schema-bit.js
+++ packages/azos-ui/models/schema-bit.js
@@ -1,11 +1,20 @@
 import { Bit } from "azos-ui/bit";
 import { html, css } from "azos-ui/ui";
+import { isBool } from "azos/types";
 
+/**
+ * SchemaBit is a custom bit that displays schema information
+ */
 export class SchemaBit extends Bit {
   static properties = {
-    source: { type: Object }
+    source: { type: Object },
+    expandedOnLoad: { type: Boolean, default: true }, // whether the bit should be expanded on load, defaults to true
+    titleOverride: { type: String }, // allows overriding the title - defaults to the schema name from the source.data.name
   }
 
+  /**
+   * Composite styles to render fields in a grid-like manner
+   */
   static styles = [Bit.styles, css`
 
     .composite {
@@ -25,108 +34,81 @@ export class SchemaBit extends Bit {
      transition-delay: .25s;
     }
 
-    .composite  az-bit[isexpanded]{ width: 100%; }
-    `];
+    .composite  az-bit[isexpanded]{
+      width: 100%;
+    }
+  `];
 
-  /**
-   * Schma bit will display the schema
-   *   main bit:
-   *     title: schema name
-   *     description: {count} fields {decription if different than name}
-   *     body:
-   *       text Account information
-   *       Metadata bit:
-   *         title: Metadata
-   *         description: none
-   *         body: current schema metadata
-   *       Repeating Field bits
-   */
+  _getSummaryData(){
 
 
-  render() {
-
+    // pull data from the source
     const attrs = this.source?.attrs && this.source.attrs.length > 0 ? this.source.attrs[0] : {};
     const fields = this.source?.fields || [];
 
+    // prepare description for the bit description
+    const subtitle = `${fields.length ?? 0} fields${attrs.description !== attrs?.name ? `: ${attrs.description}` : ""}`;
+
+    return {
+      title: `${ this.title !== this.source?.name && this.title !== this.constructor.name ? this.title : this.source?.name}`,
+      subtitle,
+      commands: []
+    };
+  }
+
+  renderDetailContent(){
+
+    // pull data from the source
+    const attrs = this.source?.attrs && this.source.attrs.length > 0 ? this.source.attrs[0] : {};
+    const fields = this.source?.fields || [];
+
+    // prepare metadata string for display
     const metadataJsonString = attrs.meta ? JSON.stringify(attrs.meta, null, 2) : "";
-    const description = `${fields.length || 0} fields${attrs.description !== attrs?.name ? `: ${attrs.description}` : ""}`;
 
-    return this.source?.fields ? html`
+    // render only when we have the information to display
+    return fields.length > 0 ? html`
+      <ul>
+        <li>Schema: ${this.source.name}</li>
+        <li>handle: ${this.source.handle}</li>
+        <li>Readonly: ${this.source.readonly ? "Yes" : "No"}</li>
+        <li>Fields: ${fields.length}</li>
+        <li>Attributes:
+          <ul>
+            ${attrs.target ? html`<li>Target: ${attrs.target}</li>` : ""}
+            ${attrs.name ? html`<li>Name: ${attrs.name}</li>` : ""}
+            ${attrs.description ? html`<li>Description: ${attrs.description}</li>` : ""}
+            ${attrs.immutable ? html`<li>Immutable: Yes</li>` :  html`<li>Immutable: No</li>`}
+          </ul>
+        </li>
+      </ul>
 
-      <az-bit
-        id="schemaBit"
-        scope="this"
-        title="${attrs.name}"
-        description="${description}"
-        isExpanded=${true}
-        status="default"
-        rank="3">
-
-        <ul>
-          <li>Schema: ${this.source.name}</li>
-          <li>handle: ${this.source.handle}</li>
-          <li>Readonly: ${this.source.readonly ? "Yes" : "No"}</li>
-          <li>Fields: ${fields.length}</li>
-          <li>Attributes:
-            <ul>
-              ${attrs.target ? html`<li>Target: ${attrs.target}</li>` : ""}
-              ${attrs.name ? html`<li>Name: ${attrs.name}</li>` : ""}
-              ${attrs.description ? html`<li>Description: ${attrs.description}</li>` : ""}
-              ${attrs.immutable ? html`<li>Immutable: Yes</li>` :  html`<li>Immutable: No</li>`}
-            </ul>
-          </li>
-        </ul>
-
-        <az-bit id="schemaMetadataBit" scope="this" title="Metadata" status="default" rank="5">
-          <az-code-box source="${metadataJsonString}"></az-code-box>
-        </az-bit>
-
-        <div class="composite">
-          ${fields ? fields.map( (field,i) => {
-              const attrs = field.attributes?.length > 0 ? field.attributes[0] || {} : {};
-              const fldDescription = attrs.description !== field.name ? `${attrs.description}` : undefined;
-              const description = [`${field.order}`, fldDescription, `${field.type}`].filter(d=>d).join(" :: ");
-
-              return html`<az-bit id="schemaFieldBit-${i}" scope="this" title="${field.name}" group="schemaFields" rank="5">
-                <ul>
-                  <li>Order: ${field.order}</li>
-                  <li>Type: ${field.type}</li>
-                  <li>GetOnly: ${field.getOnly ? "Yes" : "No"  }</li>
-                  <li>Required: ${attrs.required ? "Yes" : "No"}</li>
-                  <li>Max Length: ${attrs.maxLen || "N/A"}</li>
-                </ul>
-
-                <az-code-box scope="this" source="${JSON.stringify(attrs.meta, null, 2)}"></az-code-box>
-
-              </az-bit>`})
-            : html`<li>No fields available</li>`
-          }
-        </div>
-
+      <az-bit id="schemaMetadataBit" scope="this" title="Metadata" status="default" rank="5">
+        <az-code-box source="${metadataJsonString}"></az-code-box>
       </az-bit>
-    </div>` : html`<p>No schema data available</p>`;
+
+      <div class="composite">
+        ${fields ? fields.map( (field,i) => {
+            const attrs = field.attributes?.length > 0 ? field.attributes[0] || {} : {};
+            const fldDescription = attrs.description !== field.name ? `${attrs.description}` : undefined;
+            const description = [`${field.order}`, fldDescription, `${field.type}`].filter(d=>d).join(" :: ");
+
+            return html`<az-bit id="schemaFieldBit-${i}" scope="this" title="${field.name}" group="schemaFields" rank="5">
+              <ul>
+                <li>Order: ${field.order}</li>
+                <li>Type: ${field.type}</li>
+                <li>GetOnly: ${field.getOnly ? "Yes" : "No"  }</li>
+                <li>Required: ${attrs.required ? "Yes" : "No"}</li>
+                <li>Max Length: ${attrs.maxLen || "N/A"}</li>
+              </ul>
+
+              <az-code-box scope="this" source="${JSON.stringify(attrs.meta, null, 2)}"></az-code-box>
+
+            </az-bit>`})
+          : html`<li>No fields available</li>`
+        }
+      </div>
+    ` : html`<p>No schema data available</p>`;
   }
 }
 
 window.customElements.define("az-schema-bit", SchemaBit);
-
-
-/**
- * Example field:
- * {
- *    "name": "Id",
- *    "order": 0,
- *    "getOnly": false,
- *    "type": "string",
- *    "attributes": [
- *      {
- *        "target": "*",
- *        "description": "Account ID",
- *        "meta": "aggregatable=True aiPredictionField=False autoNumber=False byteLength=18 calculated=False calculatedFormula=null cascadeDelete=False caseSensitive=False compoundFieldName=null controllerName=null createable=False custom=False defaultValue=null defaultValueFormula=null defaultedOnCreate=True dependentPicklist=False deprecatedAndHidden=False digits=0 displayLocationInDecimal=False encrypted=False externalId=False extraTypeInfo=null filterable=True filteredLookupInfo=null formulaTreatNullNumberAsZero=False groupable=True highScaleNumber=False htmlFormatted=False idLookup=True inlineHelpText=null label=\"Account ID\" length=18 mask=null maskType=null name=Id nameField=False namePointing=False nillable=False permissionable=False polymorphicForeignKey=False precision=0 queryByDistance=False referenceTargetField=null relationshipName=null relationshipOrder=null restrictedDelete=False restrictedPicklist=False scale=0 searchPrefilterable=False soapType=tns:ID sortable=True type=id unique=False updateable=False writeRequiresMasterRead=False",
- *        "backName": "Id",
- *        "required": true,
- *        "maxLen": 18
- *      }
- *    ]
- *  },
- */
--- packages/azos-ui/test/showcase/case-schema-bit.js
+++ packages/azos-ui/test/showcase/case-schema-bit.js
@@ -4,21 +4,49 @@
  * See the LICENSE file in the project root for more information.
 </FILE_LICENSE>*/
 
-import { SelectField } from "../../parts/select-field.js";
 import { html, css } from "../../ui";
 import { CaseBase } from "./case-base";
 import "azos-ui/models/schema-bit";
 import schemaAccountDemo from "./_schema_account_example.json" assert { type: "json" };
 export class CaseSchemaBit extends CaseBase {
 
+
+  static styles = [...this.styles, css`
+    .composite {
+      margin: 1em;
+      gap: 0.5em;
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+    }
+
+    .composite az-schema-bit{
+      width: 22ch;
+      transition: 1s;
+    }
+
+    .composite az-schema-bit:not([isexpanded]){
+     transition-delay: .25s;
+    }
+
+    .composite az-schema-bit[isexpanded]{
+      width: 100%;
+    }
+  `]
+
   renderControl() {
     return html`
       <h2>Schema Bit</h2>
-      <az-schema-bit scope="this" .source="${schemaAccountDemo.data}"></az-schema-bit>
+      <az-schema-bit scope="this" .source="${schemaAccountDemo.data}" isExpanded></az-schema-bit>
+
+      <h3>8 Nested Schema Bits</h3>
+      <div class="composite">
+        ${Array.from({ length: 8 }, (_, i) => html`
+        <az-schema-bit scope="this" id="nested-schema-bit-${i}" .source="${schemaAccountDemo.data}" title="${schemaAccountDemo.data.name} (${i + 1})"></az-schema-bit>
+        `)}
+      </div>
    `;
   }
-
-
 }
 
 window.customElements.define("az-case-schema-bit", CaseSchemaBit);
