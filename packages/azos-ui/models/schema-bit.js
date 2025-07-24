1400937cb95f52c7d6da30135ebab3a11d0e3d38
Matthew O'Donnell <matthewjohnodonnell@gmail.com>
2025-07-23 23:22:55 -0400

    #473:wip schema-bit displays multi attribute for schema and fields

--- packages/azos-ui/models/schema-bit.js
+++ packages/azos-ui/models/schema-bit.js
@@ -1,15 +1,12 @@
 import { Bit } from "azos-ui/bit";
 import { html, css } from "azos-ui/ui";
-import { isBool } from "azos/types";
 
 /**
  * SchemaBit is a custom bit that displays schema information
  */
 export class SchemaBit extends Bit {
   static properties = {
-    source: { type: Object },
-    expandedOnLoad: { type: Boolean, default: true }, // whether the bit should be expanded on load, defaults to true
-    titleOverride: { type: String }, // allows overriding the title - defaults to the schema name from the source.data.name
+    source: { type: Object }
   }
 
   /**
@@ -40,15 +37,20 @@ export class SchemaBit extends Bit {
   `];
 
   _getSummaryData(){
-    const attrs = this.source?.attrs && this.source.attrs.length > 0 ? this.source.attrs[0] : {};
 
-    // prepare title and description for the bit
+    
+    // prepare title for the bit
     const isUniqueTitle = this.title !== this.source?.name && this.title !== this.constructor.name;
     const title = `${ isUniqueTitle ? this.title : this.source?.name }`;
-
-    const fieldCountDisplay = `${this.source?.fields?.length ?? 0} fields`;
+    
+    // prepare subtitle for the bit
+    const attrs = this.source?.attrs && this.source.attrs.length > 0 ? this.source.attrs[0] : {};
+    // todo: check if there is any data where there are multiple attributes
+    //  if multiple attributes are there, which do we choose for the initial description?
+    //  do we choose the initial based on the target?
+    //   target === connection ?? target === `*` <-- leaning this way but need to confirm
     const descriptionText = attrs?.description && attrs?.description !== title ? `: ${attrs.description}` : "";
-
+    const fieldCountDisplay = `${this.source?.fields?.length ?? 0} fields`;
     const subtitle = `${fieldCountDisplay}${descriptionText}`;
 
     return { title, subtitle, commands: [] };
@@ -57,11 +59,8 @@ export class SchemaBit extends Bit {
   renderDetailContent(){
 
     // pull data from the source
-    const attrs = this.source?.attrs && this.source.attrs.length > 0 ? this.source.attrs[0] : {};
-    const fields = this.source?.fields || [];
-
-    // prepare metadata string for display
-    const metadataJsonString = attrs.meta ? JSON.stringify(attrs.meta, null, 2) : "";
+    const attrs = this.source?.attrs ?? [];
+    const fields = this.source?.fields ?? [];
 
     // render only when we have the information to display
     return fields.length > 0 ? html`
@@ -70,41 +69,49 @@ export class SchemaBit extends Bit {
         <li>handle: ${this.source.handle}</li>
         <li>Readonly: ${this.source.readonly ? "Yes" : "No"}</li>
         <li>Fields: ${fields.length}</li>
-        <li>Attributes:
-          <ul>
-            ${attrs.target ? html`<li>Target: ${attrs.target}</li>` : ""}
-            ${attrs.name ? html`<li>Name: ${attrs.name}</li>` : ""}
-            ${attrs.description ? html`<li>Description: ${attrs.description}</li>` : ""}
-            ${attrs.immutable ? html`<li>Immutable: Yes</li>` :  html`<li>Immutable: No</li>`}
-          </ul>
-        </li>
+        ${attrs.map(attr => html`
+          <li>Attributes:
+            <ul>
+              ${attr.target ? html`<li><strong>Target: ${attr.target}</strong></li>` : ""}
+              ${attr.name ? html`<li>Name: ${attr.name}</li>` : ""}
+              ${attr.description ? html`<li>Description: ${attr.description}</li>` : ""}
+              ${attr.immutable ? html`<li>Immutable: Yes</li>` :  html`<li>Immutable: No</li>`}
+            </ul>
+            <az-bit id="schemaMetadataBit" scope="this" title="Metadata" status="default" rank="5">
+              <az-code-box source="${attr.meta ? JSON.stringify(attr.meta, null, 2) : ""}"></az-code-box>
+            </az-bit>
+          </li>
+        `)}
       </ul>
-
-      <az-bit id="schemaMetadataBit" scope="this" title="Metadata" status="default" rank="5">
-        <az-code-box source="${metadataJsonString}"></az-code-box>
-      </az-bit>
 
       <div class="composite">
         ${fields ? fields.map( (field,i) => {
-            const attrs = field.attributes?.length > 0 ? field.attributes[0] || {} : {};
-            const fldDescription = attrs.description !== field.name ? `${attrs.description}` : undefined;
-            const description = [`${field.order}`, fldDescription, `${field.type}`].filter(d=>d).join(" :: ");
-
             return html`<az-bit id="schemaFieldBit-${i}" scope="this" title="${field.name}" group="schemaFields" rank="5">
               <ul>
                 <li>Order: ${field.order}</li>
                 <li>Type: ${field.type}</li>
                 <li>GetOnly: ${field.getOnly ? "Yes" : "No"  }</li>
-                <li>Required: ${attrs.required ? "Yes" : "No"}</li>
-                <li>Max Length: ${attrs.maxLen || "N/A"}</li>
+                ${(field?.attributes ?? []).map(attr => html`
+                  <li>Attributes:
+                    <ul>
+                      <li>Description: ${attr.description}</li>
+                      <li>Required: ${attr.required ? "Yes" : "No"}</li>
+                      <li>Max Length: ${attr.maxLen || "N/A"}</li>
+                    </ul>
+                    <az-bit id="schemaMetadataBit" scope="this" title="Metadata" status="default" rank="5">
+                      <az-code-box source="${attr.meta ? JSON.stringify(attr.meta, null, 2) : ""}"></az-code-box>
+                    </az-bit>
+                  </li>
+                `)}
               </ul>
-
-              <az-code-box scope="this" source="${JSON.stringify(attrs.meta, null, 2)}"></az-code-box>
-
             </az-bit>`})
           : html`<li>No fields available</li>`
         }
       </div>
+
+      <az-bit title="View Raw JSON">
+        <az-code-box source="${JSON.stringify( (this?.source ?? {}) , null, 2)}"></az-code-box>
+      </az-bit>
     ` : html`<p>No schema data available</p>`;
   }
 }
